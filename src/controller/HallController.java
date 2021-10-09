c2d23b24ca2f0283a97d1835aa342d4e8c226b48
최현준 <kjh961224@gmail.com>
2021-10-09 23:41:43 +0900

    Controller 추가

---
+++ src/controller/HallController.java
@@ -0,0 +1,13 @@
+package controller;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+/**
+ * Servlet implementation class HallController
+ */
+@WebServlet("/hall/detail")
+public class HallController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+}
