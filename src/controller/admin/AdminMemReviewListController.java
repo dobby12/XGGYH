845c5968e800ce3d22f728a14c1a72c756ddb5e6
jiwonee22 <jiwonee22@naver.com>
2021-10-10 14:55:23 +0900

    수정: AdminMemReviewListController url수정

--- src/controller/admin/AdminMemReviewListController.java
+++ src/controller/admin/AdminMemReviewListController.java
@@ -3,7 +3,7 @@ package controller.admin;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 
-@WebServlet("/admin/review/list")
+@WebServlet("/admin/review/list/member")
 public class AdminMemReviewListController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
