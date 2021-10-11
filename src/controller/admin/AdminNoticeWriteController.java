691fbfb5eaf5d528de5c624dedb284fc1868a379
GARNET <GARNET@DESKTOP-ROOM>
2021-10-11 12:24:23 +0900

    서블릿 주소 변경

--- src/controller/admin/AdminNoticeWriteController.java
+++ src/controller/admin/AdminNoticeWriteController.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 import service.face.AdminNoticeService;
 import service.impl.AdminNoticeServiceImpl;
 
-@WebServlet("/admin/notice/delete")
+@WebServlet("/admin/notice/write")
 public class AdminNoticeWriteController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
