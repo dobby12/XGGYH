ef7df6c1efe1b401b22997806f21698ea05152d8
GARNET <GARNET@DESKTOP-ROOM>
2021-10-11 11:19:50 +0900

    admin logout

---
+++ src/controller/admin/AdminLogoutController.java
@@ -0,0 +1,24 @@
+package controller.admin;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebServlet("/admin/logout")
+public class AdminLogoutController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		System.out.println("###TEST### AdminLogoutController doGet()"); 
+		HttpSession session = req.getSession();
+		session.invalidate();
+		resp.sendRedirect("/XGGYH/admin");	//로그아웃 시 메인페이지로 이동	@@@수정 꼭 
+	}
+	
+}
