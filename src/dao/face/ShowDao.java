d5bea22f6da7bc94e8ec8012339c21a20bf09167
최현준 <kjh961224@gmail.com>
2021-10-11 18:01:20 +0900

    showlist 페이징 기능 추가

--- src/dao/face/ShowDao.java
+++ src/dao/face/ShowDao.java
@@ -4,8 +4,33 @@ import java.sql.Connection;
 import java.util.List;
 
 import dto.XShow;
+import util.Paging;
 
 public interface ShowDao {
 
+	/**
+	 * XShow 테이블의 정보 전체 조회
+	 * 
+	 * @param conn - DB연결 객체
+	 * @return List<XReview> - XShow테이블의 전체 조회 결과 리스트를 반환함
+	 */
 	public List<XShow> selectShowAll(Connection conn);
+
+	/**
+	 * XShow 테이블의 정보 전체 조회
+	 * 	페이징 처리 추가
+	 * 
+	 * @param paging - 페이징 정보 객체
+	 * @param conn - DB연결 객체
+	 * @return List<XReview> - XShow테이블의 전체 조회 결과 리스트를 반환함
+	 */
+	public List<XShow> selectShowAll(Connection conn, Paging paging);
+
+	/**
+	 * XShow 테이블의 전체 행 수 조회
+	 * 
+	 * @param conn - DB연결 객체
+	 * @return int - XShow 테이블의 전체 행 수가 몇개인지 반환
+	 */
+	public int selectCntAll(Connection conn);
 }
--- src/service/impl/ShowServiceImpl.java
+++ src/service/impl/ShowServiceImpl.java
@@ -2,11 +2,14 @@ package service.impl;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+
 import common.JDBCTemplate;
 import dao.face.ShowDao;
 import dao.impl.ShowDaoImpl;
 import dto.XShow;
 import service.face.ShowService;
+import util.Paging;
 
 public class ShowServiceImpl implements ShowService {
 
@@ -16,5 +19,31 @@ public class ShowServiceImpl implements ShowService {
 	@Override
 	public List<XShow> getShowList() {
 		return showDao.selectShowAll(JDBCTemplate.getConnection());
+	}
+
+	@Override
+	public List<XShow> getShowList(Paging paging) {
+		return showDao.selectShowAll(JDBCTemplate.getConnection(), paging);
+	}
+
+	@Override
+	public Paging getPaging(HttpServletRequest req) {
+		String param = req.getParameter("curPage");
+		int curPage = 0;
+		
+		//한번에 몇개씩 보여줄건지
+		int listCount = 6;
+		
+		if(param != null && !"".equals(param)) {
+			curPage = Integer.parseInt(param);
+		} else {
+			System.out.println("[WARNING] curPage값이 null이거나 비어있습니다");
+		}
+		
+		int totalCount = showDao.selectCntAll(JDBCTemplate.getConnection());
+		
+		Paging paging = new Paging(totalCount, curPage, listCount);
+		
+		return paging;
 	}
 }
