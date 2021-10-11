340652808d25338fea837d6dc2d06e52415ed803
dobby12 <yeo.dh22@daum.net>
2021-10-11 17:56:19 +0900

    fix: adminShowList 구현 완료

--- src/dao/impl/AdminShowDaoImpl.java
+++ src/dao/impl/AdminShowDaoImpl.java
@@ -1,7 +1,95 @@
 package dao.impl;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import common.JDBCTemplate;
 import dao.face.AdminShowDao;
+import dto.XShow;
+import util.Paging;
 
 public class AdminShowDaoImpl implements AdminShowDao {
+
+	PreparedStatement ps = null;
+	ResultSet rs = null;
+	
+	@Override
+	public int selectCntAll(Connection connection) {
+		String sql = "";
+		sql += "SELECT count(*) FROM xshow";
+	
+		int count = 0;
+		
+		
+		try {
+			ps = connection.prepareStatement(sql);
+			
+			rs = ps.executeQuery();
+			
+			while(rs.next()) {
+				count = rs.getInt(1);
+			}
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			JDBCTemplate.close(rs);
+			JDBCTemplate.close(ps);
+		}
+		
+		
+		return count;
+	}
+
+	@Override
+	public List<XShow> selectShowAll(Connection conn, Paging paging) {
+		
+		String sql = "";
+		sql += "SELECT * FROM (";
+		sql += "	 SELECT rownum rnum, S.* FROM (";
+		sql += "		 SELECT show_no, admin_id, show_title, show_date, show_start, show_end";
+		sql += "	 FROM xshow ORDER BY show_no DESC";
+		sql += "	 ) S";
+		sql += " ) xshow";
+		sql += " WHERE rnum BETWEEN ? AND ?";
+		
+		List<XShow> showList = new ArrayList<>();
+		
+		try {
+			ps = conn.prepareStatement(sql);
+			ps.setInt(1, paging.getStartNo());
+			ps.setInt(2, paging.getEndNo());
+			
+			rs = ps.executeQuery();
+			
+			while( rs.next() ) {
+				XShow xshow = new XShow();
+				
+				xshow.setShowNo(rs.getInt("show_no"));
+				xshow.setAdminId(rs.getString("admin_id"));
+				xshow.setShowTitle(rs.getString("show_title"));
+				xshow.setShowDate(rs.getDate("show_date"));
+				xshow.setShowStart(rs.getDate("show_start"));
+				xshow.setShowEnd(rs.getDate("show_end"));
+				
+				showList.add(xshow);
+				
+			}
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			JDBCTemplate.close(rs);
+			JDBCTemplate.close(ps);
+		}
+		
+		
+		return showList;
+		
+	}
 
 }
--- src/service/impl/AdminShowServiceImpl.java
+++ src/service/impl/AdminShowServiceImpl.java
@@ -1,11 +1,14 @@
 package service.impl;
 
+import java.sql.Connection;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 
+import common.JDBCTemplate;
 import dao.face.AdminShowDao;
 import dao.impl.AdminShowDaoImpl;
+import dto.XAsk;
 import dto.XShow;
 import service.face.AdminShowService;
 import util.Paging;
@@ -16,14 +19,31 @@ public class AdminShowServiceImpl implements AdminShowService {
 	
 	@Override
 	public Paging getPaging(HttpServletRequest req) {
-		// TODO Auto-generated method stub
-		return null;
+		
+		String param = req.getParameter("curPage");
+		
+		int curPage = 0;
+		
+		if(param != null && !"".equals(param)) {
+			curPage = Integer.parseInt(param);
+		} else {
+			System.out.println("[WARNING] curPage값이 null입니다.");
+		}
+		
+		int totalCount = adminShowDao.selectCntAll(JDBCTemplate.getConnection());
+		
+		Paging paging = new Paging(totalCount, curPage);
+		
+		return paging;
 	}
 
 	@Override
 	public List<XShow> getShowList(Paging paging) {
-		// TODO Auto-generated method stub
-		return null;
+		Connection conn = JDBCTemplate.getConnection();
+		
+		List<XShow> list = adminShowDao.selectShowAll(conn, paging);
+		
+		return list;
 	}
 
 }
