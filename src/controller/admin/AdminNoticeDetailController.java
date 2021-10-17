73403049dcfe7208b99f0d3f5635f30676e13325
GARNET <GARNET@DESKTOP-ROOM>
2021-10-17 13:59:44 +0900

    승인된 관리자 로그인 상태 아닐 시 모두 /admin으로 리디렉션

--- src/controller/admin/AdminNoticeDetailController.java
+++ src/controller/admin/AdminNoticeDetailController.java
@@ -25,7 +25,7 @@ public class AdminNoticeDetailController extends HttpServlet {
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		System.out.println("승인된 관리자 로그인 상태 : "+adminService.authorAdmin((String)req.getSession().getAttribute("adminid")));
 		if(!adminService.authorAdmin((String)req.getSession().getAttribute("adminid"))) {			
-			resp.sendRedirect("/admin/notice/list");
+			resp.sendRedirect("/admin");
 			return;
 		}
 		System.out.println("###TEST### AdminNoticeDetailController Get");
--- src/controller/admin/AdminNoticeUpdateController.java
+++ src/controller/admin/AdminNoticeUpdateController.java
@@ -26,7 +26,7 @@ public class AdminNoticeUpdateController extends HttpServlet {
 		
 		System.out.println("승인된 관리자 로그인 상태 : "+adminService.authorAdmin((String)req.getSession().getAttribute("adminid")));
 		if(!adminService.authorAdmin((String)req.getSession().getAttribute("adminid"))) {			
-			resp.sendRedirect("/admin/notice/list");
+			resp.sendRedirect("/admin");
 			return;
 		}
 		
